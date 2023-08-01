64a958813599e50762ef88b0c24370187e93436b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-01 09:21:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# db-teamcity-1aug2023
+ TeamCity demo code for Deutsche Bank trainees 1 Aug 2023
---
+++ src/main/java/com/db/demo/App.java
@@ -0,0 +1,14 @@
+package com.db.demo;
+
+/**
+ * @author Vaman Deshmukh
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+	}
+}
